86a4359e463cb8ea809c1f93f5a4e6b7909fd421
huazhouwang <huazhou19@gmail.com>
2021-11-22 16:00:17 +0800

    feat: demo data for yearn

---
+++ src/providers/yearn/index.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+const URL = 'https://api.yearn.finance/v1/chains/1/vaults/all';
+
+const run = async () => {
+  const resp = await fetch(URL).then((i) => i.json());
+  return resp.map(({ token, tvl, apy }) => ({
+    symbol: token.symbol,
+    address: token.address,
+    icon: token.icon,
+    tvl: tvl.tvl,
+    apy: apy['net_apy'],
+  }));
+};
+
+module.exports = {
+  version: 1,
+  run,
+};
