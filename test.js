df3469fdf7aa114ed418f01730a3c6c14f2f2eaf
swan <swan@me.com>
2021-11-24 13:17:15 +0800

    feat: add venus(api) & pancake(chain)

---
+++ test.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+if (process.argv.length < 3) {
+  console.error(`Missing argument, you need to provide the filename of the adapter to test.
+    Eg: node test.js src/providers/{project}/myadapter.js`);
+  process.exit(1);
+}
+const passedFile = path.resolve(process.cwd(), process.argv[2]);
+
+(async () => {
+  const m = require(passedFile);
+  const apy = await m.run();
+  if (apy !== null) {
+    apy.forEach((item) => {
+      console.log(item.symbol, item.address, item.tvl, item.apy);
+    });
+  }
+
+  process.exit(0);
+})();
