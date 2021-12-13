aba1e3b203f3960aa015df76b7f1e0ddea7db5c0
swan <swan@me.com>
2021-12-13 22:41:58 +0800

    feat: remove yearn dup vault

--- src/providers/yearn/eth.js
+++ src/providers/yearn/eth.js
@@ -3,14 +3,41 @@ const URL = 'https://api.yearn.finance/v1/chains/1/vaults/all';
 
 const run = async () => {
   const resp = await fetch(URL).then((i) => i.json());
-  return resp.map(({ token, tvl, apy }) => ({
-    name: token.symbol,
-    address: token.address,
-    depositCoins: [token.address],
-    tvl: tvl.tvl,
-    apy: apy['net_apy'],
-    lp: false,
-  }));
+  let pools = [];
+  let ids = new Map();
+  resp.forEach(({ type, inception, token, version }) => {
+    if (type !== 'v2') {
+      return;
+    }
+    const addr = token.address.toLowerCase();
+    if (!ids.has(addr)) {
+      ids.set(addr, [inception, version]);
+    } else {
+      if (ids.get(addr)[1] < version) {
+        ids.set(addr, [inception, version]);
+      }
+    }
+  });
+
+  resp.forEach(({ inception, token, tvl, apy }) => {
+    const addr = token.address.toLowerCase();
+    if (!ids.has(addr)) {
+      return;
+    }
+    if (ids.get(addr)[0] !== inception) {
+      return;
+    }
+
+    pools.push({
+      name: token.symbol,
+      address: addr,
+      depositCoins: [addr],
+      tvl: tvl.tvl,
+      apy: apy['net_apy'],
+      lp: false,
+    });
+  });
+  return pools;
 };
 
 module.exports = {
