10663513e1d31dab5d05e30f046a15e835bb6944
swan <swan@me.com>
2021-12-15 11:03:52 +0800

    fix: add geist lending apr

--- src/providers/geist/ftm.js
+++ src/providers/geist/ftm.js
@@ -11,6 +11,20 @@ const erc20Abi = new ethers.utils.Interface([
   'function symbol() view returns (string memory)',
 ]);
 
+const LENDING_POOL_ADDR = '0x9FAD24f572045c7869117160A571B2e50b10d068';
+
+const E18 = ethers.BigNumber.from('1000000000000000000'); // 1e18
+
+const lendingPoolAbi = new ethers.utils.Interface([
+  'function getReserveData(address) view returns (tuple(uint), uint128, uint128, uint128, uint128, uint128, uint40, address, address, address, address, uint8)',
+]);
+
+const lendingPoolContract = new ethers.Contract(
+  LENDING_POOL_ADDR,
+  lendingPoolAbi,
+  provider,
+);
+
 // TODO add lending apy
 const run = async () => {
   const resp = await fetch(URL).then((i) => i.json());
@@ -24,10 +38,14 @@ const run = async () => {
         erc20Abi,
         provider,
       );
+      let reserveData = await lendingPoolContract.getReserveData(
+        item.underlyingAsset,
+      );
+      let lendingApr = reserveData[3].div(E18).toNumber() / 1e9;
       let name = await tokenContract.symbol();
       pools.push({
         tvl: item.poolValue,
-        apy: item.apy,
+        apy: item.apy + lendingApr,
         address: item.underlyingAsset,
         depositCoins: [item.underlyingAsset],
         lp: false,
